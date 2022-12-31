ca92eac1d2ac92be7364a568d59747f6d74b27b6
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-12-30 19:41:22 -0500

    [ADD] Template strings

--- 01-typescript-intro/src/basics/01-types.ts
+++ 01-typescript-intro/src/basics/01-types.ts
@@ -1,3 +1,16 @@
-export const name: string = 'Stiven';
+export let name: string = 'Stiven';
 export const age: number = 24;
 export const isValid: boolean = true;
+
+name = 'Melissa';
+// name = 123;
+// name = true;
+
+export const templateString = ` This a multiline string
+that can have "", '', inject values ${name}, 
+expressions: ${1 + 1}
+numbers ${age}
+booleans: ${isValid}
+`;
+
+console.log(templateString);
