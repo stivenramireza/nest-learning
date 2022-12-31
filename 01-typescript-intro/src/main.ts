5fa44809927bceb81494ea85c8b6ce6a05090274
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-12-30 19:53:47 -0500

    [ADD] Interfaces

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,8 +1,7 @@
 import './style.css';
 import typescriptLogo from './typescript.svg';
 import { setupCounter } from './counter';
-
-import { name, age } from './basics/01-types';
+import { bubasaur } from './basics/02-objects';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -12,7 +11,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript + ${name} + ${age}</h1>
+    <h1>Vite + TypeScript + ${bubasaur.name}</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
