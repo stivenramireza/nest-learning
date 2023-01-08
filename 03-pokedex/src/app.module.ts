a9c5d84350e8039e36d1ddd9c5bcae964fd9d00d
Stiven Ramírez Arango <stivenramireza@gmail.com>
2023-01-08 11:52:39 -0500

    [ADD] Pokedex API boilerplate

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -1,6 +1,15 @@
+import { join } from 'path';
+
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonsModule } from './pokemons/pokemons.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    PokemonsModule,
+  ],
 })
 export class AppModule {}
