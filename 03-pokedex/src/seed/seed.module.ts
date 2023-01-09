173419d0472dbaff29e562c540d08d65130fc71c
Stiven Ramírez Arango <stivenramireza@gmail.com>
2023-01-09 16:01:21 -0500

    [ADD] Pokemons data insertion using chunks

--- 03-pokedex/src/seed/seed.module.ts
+++ 03-pokedex/src/seed/seed.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { PokemonsModule } from 'src/pokemons/pokemons.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
+  imports: [PokemonsModule],
 })
 export class SeedModule {}
--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,22 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
+import { PokemonsService } from 'src/pokemons/pokemons.service';
 import { PokeApiResponse } from './interfaces/poke-api-response.interface';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(private readonly pokemonsService: PokemonsService) {}
+
   async executeSeed() {
     const { data } = await this.axios.get<PokeApiResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=500',
     );
 
-    data.results.forEach(({ name, url }) => {
-      const segments = url.split('/');
-      const no: number = +segments[segments.length - 2];
-      console.log(name, no);
-    });
+    const results = data.results;
 
-    return data.results;
+    let counter = 0;
+    const chunks = 20;
+
+    while (counter <= results.length) {
+      const data = results.slice(counter, counter + chunks);
+
+      data.forEach(async ({ name, url }) => {
+        const segments = url.split('/');
+        const no: number = +segments[segments.length - 2];
+        const pokemon = { name, no };
+
+        await this.pokemonsService.create(pokemon);
+      });
+
+      counter = counter + chunks;
+    }
+
+    return 'Seed executed';
   }
 }
