05a3d65e978041f8aeed5989e6fb52fe4e87a29a
Stiven Ramírez Arango <stivenramireza@gmail.com>
2023-01-22 12:01:37 -0500

    [ADD] Better database config

--- 04-teslo-shop/src/app.module.ts
+++ 04-teslo-shop/src/app.module.ts
@@ -1,20 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import config, { ConfigSchema } from './config/config';
+import { TypeOrmConfigService } from './config/database.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+      validationSchema: ConfigSchema,
     }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
   ],
   controllers: [],
   providers: [],
